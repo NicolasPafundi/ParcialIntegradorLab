d8735399f0dec5491f01c37c036b1680bfb02087
NicolasPafundi <nicolaspafundi95@gmail.com>
2020-06-24 19:49:54 -0300

    Parcial laboratorio integrador

--- src/main/java/com/example/ParcialIntegrador/Controller/PhoneLineController.java
+++ src/main/java/com/example/ParcialIntegrador/Controller/PhoneLineController.java
@@ -1,7 +1,11 @@
 package com.example.ParcialIntegrador.Controller;
 
 import com.example.ParcialIntegrador.Service.Integration.IntegrationService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +17,15 @@ public class PhoneLineController {
     @Autowired
     IntegrationService integrationService;
 
-    @GetMapping("/pet")
-    public int getCantCellphoneLines() {
-        return integrationService.getCantCellphoneLines();
+    @GetMapping("/PhoneLine")
+    @ApiOperation(value="obtener la cantidad lineas telefonicas moviles")
+    @ApiResponses(value={@ApiResponse(code=200,message = "ejecucion exitosa"), @ApiResponse(code = 500, message = "error interno")})
+    public ResponseEntity<Integer> getCantCellphoneLines() {
+        try{
+            return ResponseEntity.ok(integrationService.getCantCellphoneLines());
+        }catch (Exception ex){
+            throw ex;
+        }
+
     }
 }
